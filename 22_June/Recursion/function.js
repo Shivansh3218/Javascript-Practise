628546cc02a9c6b3f1e96298169bf7068bc92602
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-06-22 21:37:01 +0530

    Recursion practise

---
+++ 22_June/Recursion/function.js
@@ -0,0 +1,32 @@
+//Function to print the factorial of a number
+
+
+// function y(num, fact){
+//    if(num>0){
+//     fact =fact*num
+//     return y(num-1,fact)
+//    }
+//    return fact
+// }
+// console.log(y(5,1))
+
+
+
+
+// To print hello infinite times
+// function y(n){
+//     console.log("HELLO")
+//     return y()
+// }
+// y()
+
+//TO CONTROL THE TIMES HELLO IS PRINTED TRYING FOR 2 TIMES
+
+function y(n){
+  
+    if(n<10){
+        console.log("HELLO")
+    return y(n+1)
+}
+}
+y(8)
