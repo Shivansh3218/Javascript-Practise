3252654576353e824791c4edabc99b37382cca4e
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-06-23 17:34:07 +0530

    Anonymous practise

---
+++ 23_June/Anonymous_function/anonymous.js
@@ -0,0 +1,8 @@
+TRYING ANONYMOUS FUNCTION FOR FIRST TIME FOR MULTIPLICATION OF 2 NUMBERS
+
+
+let abc = function (a,b){// function defined with no name and stored in a variable
+    return a*b
+}
+console.log(abc(10,15))// Calling a function
+
