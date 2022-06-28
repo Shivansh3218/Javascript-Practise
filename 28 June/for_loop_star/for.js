2cb71d872ecc03247461e0acb80fa9a0bc5f6151
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-06-28 12:02:02 +0530

    Practise on for series with stars

---
+++ 28 June/for_loop_star/for.js
@@ -0,0 +1,9 @@
+for(let i=1; i<=3;i++){
+    for(let j=1; j<=3;j++){
+        console.log("*")  //this loop will print 3 stars in different lines
+
+    }
+    console.log("+++++") // this is printing ++++ signs every time the j loop ends
+    // this for loop will continue untill the condition in for is satisfied
+    
+}
