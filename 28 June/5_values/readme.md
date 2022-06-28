99199c644b32798f7784c6e38edd5deae64e092c
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-06-28 12:39:24 +0530

    Printing in squaere format

---
+++ 28 June/5_values/readme.md
@@ -0,0 +1,2 @@
+# The output using console in different lines
+# <img src = "2.png"
---
+++ 28 June/5_values/val.js
@@ -0,0 +1,14 @@
+for(let i =1; i<=4;i++){// Here i defines number of rows 
+    let empty =' ' // adding empty string is compulsory
+    for (let j =1;j<=5;j++){// This defines how many columns are there 
+        empty = empty+ j // this is adding j to make columns 
+        // console.log(empty)    
+        
+        // 1 in first line tghen 1 2 in second 1 2 3 in third likewise
+        // so if i want a triangle i would print it here 
+    }
+     console.log(empty)
+     // This prints all in a same line with same columns
+    // Printed if I want output in the box format
+
+}
