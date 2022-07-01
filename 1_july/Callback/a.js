d1ce0d4761219cde10fc5be1387269520a04b45f
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-01 11:50:02 +0530

    In class revison

---
+++ 1_july/Callback/a.js
@@ -0,0 +1,10 @@
+function inhotel(message,callback){
+//    callback ="Helllo"
+    // console.log(callback)
+    console.log(message)
+}
+    function order(){
+        console.log("Order the food")
+    }
+
+    inhotel("Hello",order())
