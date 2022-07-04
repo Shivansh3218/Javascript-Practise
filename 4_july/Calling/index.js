4f67dc9c3476f9d9888c4b9a867f0366d1e554c4
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-04 22:48:37 +0530

    calling

---
+++ 4_july/Calling/index.js
@@ -0,0 +1,29 @@
+function order(message){   
+    return message
+  }
+  function process(message1){
+    return message1
+  }
+  function ready(message2){
+    return message2
+  }
+  function flavours(message3){
+    return message3
+  }
+  function cooked(message4){
+    return message4
+  }
+  function ready(message5){
+    return message5
+  }
+  function delivered(message6){
+    return message6 
+  }
+  
+  console.log(order(" The pizza is ordered"))
+  console.log(process(" The pizza order is processing"))
+  console.log(ready(" The ingredients are ready"))
+  console.log(flavours(" The flavours are added"))
+  console.log(cooked(" The pizza is cooked and baked"))
+  console.log(ready(" The pizza is ready and out for delivery"))
+  console.log(delivered(" The pizza is recieved"))
