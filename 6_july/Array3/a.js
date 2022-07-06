4cf21bb44421135a353f40e8b12d62702c76c475
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-06 11:09:55 +0530

    Shift and unshift

--- 6_july/Array3/a.js
+++ 6_july/Array3/a.js
@@ -1,6 +1,9 @@
 let nums = ['a','b','c','d']
 nums.push("shivansh")
 // console.log(nums)
-nums.pop()
-console.log(nums)
+// nums.pop()
+// console.log(nums)
 // console.log(nums.pop())
+// nums.shift()
+nums.unshift("Ajay")
+console.log(nums)
