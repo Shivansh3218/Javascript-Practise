d430f6c4fa7793116b280b123a6c52925eae5f3e
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-06 10:45:30 +0530

    Practise

---
+++ 6_july/Array1/a.js
@@ -0,0 +1,11 @@
+let nums = [1,2,3,4,5,54,7,8,9]
+// console.log(nums)
+let a = nums.length
+// console.log(a)
+// console.log(nums.length-1)
+nums.splice(2,2, "shiv")
+// console.log(nums)
+
+cnums.slice(2,2)
+
+
