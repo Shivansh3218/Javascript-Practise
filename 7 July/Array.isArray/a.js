80d91760b7816acb0cba336de30aad5dafe2fbc5
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-07 07:35:13 +0530

    Checked all datatypes

---
+++ 7 July/Array.isArray/a.js
@@ -0,0 +1,30 @@
+let arr = [1,2,3,4,5]
+let num = "Shivansh"
+let name
+let arm =123
+let sum = Array.isArray(arr)
+let pro = null
+let abc = {
+    name : "Shiv", age :22, status: "High"
+}
+ let shiva = function (a,b){
+    let sum = a+b
+    return sum
+
+}
+    
+shiva(5,20)
+
+
+
+console.log(Array.isArray(arr)) //Returns Boolean value
+
+console.log(typeof(arr))
+console.log(typeof(num))
+console.log(typeof(name))
+console.log(typeof(arm))
+console.log(typeof(sum))
+console.log(typeof(pro))
+console.log(typeof(abc))
+console.log(typeof(shiva))
+console.log(abc['age'])
