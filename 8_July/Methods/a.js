c7438bd3356307f98393111c620bc952e846ce2e
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-08 22:01:20 +0530

    Methods of array

---
+++ 8_July/Methods/a.js
@@ -0,0 +1,9 @@
+let num = [1,2,3,45,6,3,5,23,2]
+
+console.log(num.indexOf(45))
+console.log(num.lastIndexOf(45))
+console.log(Array.isArray(num))
+console.log(num.reverse())
+console.log(num.includes(45))
+
+
