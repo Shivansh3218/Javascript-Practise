6d746343fe0669cf6b0cd7002415c43f99234582
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-09 11:12:22 +0530

    practise

---
+++ 8_July/Filter/a.js
@@ -0,0 +1,15 @@
+let arr = [1,2,3,4,5,6,7]
+
+let arr2 = arr.map(shiv);
+function shiv(num){
+    // console.log(num)
+    console.log( num*num)
+    // console.log(num === 4 ? "ok" : "not ok")
+
+}
+// let a =  (num)=>{
+//     console.log(num)
+// }
+// let newArr= arr.filter(a())
+
+// console.log(newArr)
