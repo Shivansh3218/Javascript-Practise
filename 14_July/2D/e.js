fc93ff3e57478c0b27d36c45b8404e4e62be5383
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-14 12:10:28 +0530

    Print only first elements of subarray

---
+++ 14_July/2D/e.js
@@ -0,0 +1,10 @@
+let arr = [
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+    [11,12,13,14,15],
+];
+
+for(let i = 0; i < arr.length; i++){
+    // for(let j = 0; j <=i ; j++){
+        console.log(arr[i][0]);
+    }
