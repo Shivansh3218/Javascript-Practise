9e49d41e1832c47d745640d4feda6ea995ca3b9c
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-17 09:25:33 +0530

    Practise

--- 17_July/Practise/a.js
+++ 17_July/Practise/a.js
@@ -1,1 +1,9 @@
-console.log("Starting")
+// console.log("Starting")
+
+let str = "RawaT"
+
+for(let i = 0; i < str.length; i++){
+    if(str[i]== str[i].toUpperCase()){
+        console.log("HELLO", str[i])
+    }
+}
