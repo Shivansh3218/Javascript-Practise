0c4df1bf47639a0e2ae85e427a783a506c99b4b4
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-24 13:01:22 +0530

    timeout practise

---
+++ 24_july/timeout.js
@@ -0,0 +1,10 @@
+let id = setInterval(function(){
+    console.log("This is a timeout function")
+}, 2000)
+
+// clearInterval(id)
+
+
+// let classs = setTimeout(function(){
+//     console.log("Classsing...");
+// },  3000)
