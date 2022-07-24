32bad0d2be2e36e9d972d87318c590c13be47a57
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-24 20:48:59 +0530

    Create d.js

---
+++ 24_july/d.js
@@ -0,0 +1,21 @@
+//  function joy(a,b){
+//  let sum = a+b
+//  return sum
+// }
+// // console.log(a(12,42))
+
+// function xyz(joy){
+//   return  joy(12,23)
+// }
+// console.log(xyz())
+
+function a (text){
+ text = "Hello, world!"
+ return text
+    // console.log("hello world")
+}
+function b(text){
+   a(text);
+}
+// b()
+console.log(b())
