fda56a3345f0543b663e1181d8f3fb46015fa76c
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-24 11:13:33 +0530

    For of, in and normal for loop practise

--- 24_july/a.js
+++ 24_july/a.js
@@ -1,13 +1,19 @@
-// let arr = [ 
-//     {name :"Shiv", age : 22 },
-//     {name :"Shiv", age : 22 },
-//     {name :"Shiv", age : 22}
-// ]
-// for(let i of arr){
-//     console.log(arr[i])
+let arr = [ 
+    {name :"Shiv", age : 22 },
+    {name :"Shiv", age : 22 },
+    {name :"Shiv", age : 22}
+]
+for(let i of arr){
+    console.log(i)
 
-// }
+}
+for(let i in arr ){
+    console.log(arr[i])
+}
 
+for(let i =0;i<arr.length;i++){
+    console.log(i)
+}
 
 // let arr = [ 1,1,2,3,4,5,6,7,8,9]
 // for(let i of arr){
