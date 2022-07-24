e1d9ce3be34817906e891f8645e08fc8cf471aff
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-24 12:45:26 +0530

    HOF function

---
+++ 24_july/c.js
@@ -0,0 +1,12 @@
+ function abc(input){
+   console.log(input)
+}
+function xyz(input){
+    abc(input)
+}
+(xyz("Shivansh is a special Person"))
+abc("Pratibha is doing M.Sc")
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+// arr.sort(function (a, b) {return a-b})
