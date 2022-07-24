c8fb581d1ab9294aa9cedb5dec24656081fb231d
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-24 11:06:32 +0530

    Practise of array of objects sorting

---
+++ 24_july/a.js
@@ -0,0 +1,16 @@
+// let arr = [ 
+//     {name :"Shiv", age : 22 },
+//     {name :"Shiv", age : 22 },
+//     {name :"Shiv", age : 22}
+// ]
+// for(let i of arr){
+//     console.log(arr[i])
+
+// }
+
+
+// let arr = [ 1,1,2,3,4,5,6,7,8,9]
+// for(let i of arr){
+//     console.log(i)
+
+// }
