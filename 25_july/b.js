6c8476bb5e469d33ae6123c113966273c1bf24fd
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-25 18:34:54 +0530

    Destructuring

---
+++ 25_july/b.js
@@ -0,0 +1,21 @@
+//Array Destructuring 
+let arr = [1,2,3,4,4,5]
+let [num1, num2]= arr
+// console.log(num1, num2)
+
+//Advance Destructuring of array  by assigning values to elements
+
+let Person = [ "Shivansh ", 22, "Srinagar"]
+
+let [name , age , city] = Person
+
+// console.log(name, age, city)
+
+// For nested arrays
+
+let nested = [ "Prti", "microbiology", [ "No ", "Yes"]]
+
+let  [Id, dept , [Married , working]]= nested
+
+// console.log(working)
+
