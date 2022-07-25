693beb0078bfc121ab7599a7dc5832ccc131444b
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-25 20:59:37 +0530

    Object Constructor

---
+++ 25_july/c.js
@@ -0,0 +1,10 @@
+function obj(name, age) {
+this.name =name,
+this.age = age
+}
+var student1= new obj("Shivansh", 22)
+var student2 = new obj("Prati", 21)
+var student3 = new obj("Subee", 23)
+console.log(student1)
+console.log(student2)
+console.log(student3)
