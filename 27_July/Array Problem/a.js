00b798a0b9e034a268e003325dbc7b2dbab9cd41
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-07-27 13:17:26 +0530

    Update a.js

--- 27_July/Array Problem/a.js
+++ 27_July/Array Problem/a.js
@@ -1,6 +1,6 @@
-//Find the second largest element in the array and return it if is present else return false.
+//Find the second largest element in the array and return it.
 
-let arr = [1,3,2,34,3]
+let arr = [1,3,2,34,30]
 for(let i=0;i<arr.length;i++){ //Bubble sort compares the element from index 0 and if the 0th index is greater than 1st index then the values get swapped and if the 0th index is less than the 1st index then nothing happens.
 for(let j=0;j<arr.length-i-1;j++){
     if(arr[j] > arr[j+1]){
@@ -10,4 +10,6 @@ for(let j=0;j<arr.length-i-1;j++){
         arr[j+1] = temp
       }
     }
-}console.log(arr)
+}
+// }console.log(arr)
+console.log(arr[arr.length-2])
