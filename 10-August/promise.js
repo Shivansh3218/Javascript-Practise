e9dac3b220e85fde5b79da76cb3d34e66e6068dd
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-08-10 17:31:13 +0530

    Promise

---
+++ 10-August/promise.js
@@ -0,0 +1,9 @@
+let momPromise = new Promise ((resolve, reject)=>{
+    setTimeout(()=>{
+        resolve = ("Collected all of gold");
+        reject = ("Killed by Adheera");
+    },1000);
+});
+momPromise
+.then((result )=>console.log("Climax" , result))
+.catch((error)=>console.log(error)) ;
