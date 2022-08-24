81dc0c7d9cc95aa0a1ba2a743f7358d1fcc452d7
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-08-24 13:31:17 +0530

    Map polyfill for power of 2

--- 24-Aug/b.js
+++ 24-Aug/b.js
@@ -1,8 +1,18 @@
 //MAP POLYFILL BASIC======---------
- Array.prototype.myMap = function(){
-    console.log(this) //This refers to entire array
- }
- let num = [1,2,3]
- num.myMap()
+//  Array.prototype.myMap = function(){
+//     console.log(this) //This refers to entire array
+//  }
+//  let num = [1,2,3]
+//  num.myMap()
 
- 
+ //--------A Mapped function to get the power of 2 of each element of the array
+
+ Array.prototype.myMap = function(){
+    let result = []
+    for(let i=0;i<this.length;i++){
+       result.push(this[i]**2) 
+    }
+    return result
+ }
+ let num =[1,2,4,5]
+ console.log(num.myMap());
