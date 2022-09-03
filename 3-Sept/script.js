325a61c12aa3f0c3422eac42201be8fdaaf232c5
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-03 21:58:45 +0530

    Update script.js

--- 3-Sept/script.js
+++ 3-Sept/script.js
@@ -2,7 +2,7 @@ let carousel = [
   {
     id:1,
     title:"boAt Airdopes 131...",
-    reviews: 1116 ,
+    reviews: "1116 reviews" ,
     image:"/file/img-1-1.webp",
     price:"Rs 1,299",
     discount:"Rs 1691 (57%)"
@@ -10,7 +10,7 @@ let carousel = [
   {
     id:2,
     title:"boAt Watch Xtend...",
-    reviews: 121 ,
+    reviews:"121 reviews" ,
     image:"/file/img-2-1.webp",
     price:"Rs 2,999",
     discount:" Rs 4991 (62%)"
@@ -18,7 +18,7 @@ let carousel = [
   {
     id:3,
     title:"boAt Airdopes 141...",
-    reviews: 411 ,
+    reviews: "411 reviews" ,
     image:"/file/img-3-1.webp",
     price:"Rs 1,499",
     discount:"Rs 2991 (67%)"
@@ -26,7 +26,7 @@ let carousel = [
   {
     id:4,
     title:"boAt Rockerz 245 V2",
-    reviews: 189 ,
+    reviews: "189 reviews" ,
     image:"/file/img-4-1.webp",
     price:"Rs 999",
     discount:"Rs 1991 (67%)"
@@ -42,8 +42,7 @@ let displayCarousel =function(){
         <img class="best-seller-image" src="${item.image}" alt="">
         <div class="inside-best-seller">
             <h3>${item.title}</h3>
-            <p>1116 reviews</p>
-            <hr>
+            <p>${item.reviews}</p>
             <h5>${item.price}</h5>
             <p class="save">${item.discount}</p>
             <button>Add to Cart</button>
