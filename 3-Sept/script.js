01f0696f3c193f73fd3ecf146353ec86a8725ff3
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-03 21:56:30 +0530

    Container carousel 1st commit

---
+++ 3-Sept/script.js
@@ -0,0 +1,56 @@
+let carousel = [
+  {
+    id:1,
+    title:"boAt Airdopes 131...",
+    reviews: 1116 ,
+    image:"/file/img-1-1.webp",
+    price:"Rs 1,299",
+    discount:"Rs 1691 (57%)"
+  },
+  {
+    id:2,
+    title:"boAt Watch Xtend...",
+    reviews: 121 ,
+    image:"/file/img-2-1.webp",
+    price:"Rs 2,999",
+    discount:" Rs 4991 (62%)"
+  },
+  {
+    id:3,
+    title:"boAt Airdopes 141...",
+    reviews: 411 ,
+    image:"/file/img-3-1.webp",
+    price:"Rs 1,499",
+    discount:"Rs 2991 (67%)"
+  },
+  {
+    id:4,
+    title:"boAt Rockerz 245 V2",
+    reviews: 189 ,
+    image:"/file/img-4-1.webp",
+    price:"Rs 999",
+    discount:"Rs 1991 (67%)"
+  },
+
+]
+
+let container = document.querySelector(".container")
+let displayCarousel =function(){
+    carousel.map((item)=>{
+        // console.log(item)
+       let html = ` <div class="best-seller-div">
+        <img class="best-seller-image" src="${item.image}" alt="">
+        <div class="inside-best-seller">
+            <h3>${item.title}</h3>
+            <p>1116 reviews</p>
+            <hr>
+            <h5>${item.price}</h5>
+            <p class="save">${item.discount}</p>
+            <button>Add to Cart</button>
+        </div>
+    </div>`
+    container.innerHTML +=html
+    })
+}
+    displayCarousel(carousel)
+
