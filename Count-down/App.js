696cd1d1210d112c3148cad4c4d12d719b7867cf
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-09 21:21:01 +0530

    Update App.js

--- Count-down/App.js
+++ Count-down/App.js
@@ -1,23 +1,15 @@
-const items = document.querySelectorAll('.flash-sale-end-format h4');
+const itemsOfTimer = document.querySelectorAll('.flash-sale-end-format h4');
 
 let tempDate = new Date();
 let tempYear = tempDate.getFullYear();
 let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDate();
 
-// months are ZERO index based;
 const futureDate = new Date(tempYear, tempMonth, tempDay+1, 12, 00, 00);
-// console.log(futureDate)
-const year = futureDate.getFullYear();
-const hours = futureDate.getHours();
-const minutes = futureDate.getMinutes();
-let month = futureDate.getMonth();
 const date = futureDate.getDate();
-
 const futureTime = futureDate.getTime();
-function getRemaindingTime() {
+function getRemainingTime() {
   const today = new Date().getTime();
-
   const t = futureTime - today;
 
   const oneDay = 24 * 60 * 60 * 1000;
@@ -30,19 +22,17 @@ function getRemaindingTime() {
   let minutes = Math.floor((t % oneHour) / oneMinute);
   let seconds = Math.floor((t % oneMinute) / 1000);
   let milisecond = Math.floor((t%100)/10)
-  console.log(milisecond)
 
-  // setting values array
+  // putting values in the array to pass inside the timer
 
-  const values = [ hours, minutes, seconds, milisecond];
-  //If time is less than 10 then add 0 before it
+  const timerValue = [ hours+"hr", minutes+"mins", seconds+" s", milisecond];
   function format(item) {
     return item;
   }
 //For each selects each item  to attach on the count down timer
-  items.forEach(function (item, i) {
-    item.innerHTML = format(values[i]);
+  itemsOfTimer.forEach(function (item, i) {
+    item.innerHTML = format(timerValue[i]);
   });
 }
-let countdown = setInterval(getRemaindingTime, 10);
-getRemaindingTime();
+let countdown = setInterval(getRemainingTime, 70);
+getRemainingTime();
