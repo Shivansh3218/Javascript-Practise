ba8466984567e2739b9e539dd93c68aed39ff9a4
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-28 13:51:40 +0530

    Update d.js

--- LeetCode/d.js
+++ LeetCode/d.js
@@ -6,3 +6,18 @@
 // - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
 // - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
 // - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+let candies = [4,2,1,1,2]
+let extraCandies = 1
+let sortedCandies = [...candies]
+candies.sort()
+let arr= []
+let max = candies[candies.length-1]
+for(let i=0;i<candies.length;i++){
+    if(sortedCandies[i]+extraCandies>=max){
+        console.log(candies[i]+extraCandies,max)
+        arr.push(true)
+    }else arr.push(false)
+}
+console.log(arr)
+return arr
