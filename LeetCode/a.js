ffaf047b4d8e5b4240d3265389155cbdec5ca84a
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-28 12:12:45 +0530

    Create a.js

---
+++ LeetCode/a.js
@@ -0,0 +1,19 @@
+// Input: operations = ["--X","X++","X++"]
+// Output: 1
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// --X: X is decremented by 1, X =  0 - 1 = -1.
+// X++: X is incremented by 1, X = -1 + 1 =  0.
+// X++: X is incremented by 1, X =  0 + 1 =  1.
+
+var finalValueAfterOperations = function(operations) {
+    let result =0
+    for(let i =0 ;i<operations.length;i++){
+         if(operations[i]=="X++"|| operations[i]=="++X"){
+            result++
+        }else{
+            result--
+        }
+    }
+   return result
+};
