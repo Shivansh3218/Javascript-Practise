453a090507a3b13b54f5b1a10900067386f0349e
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-28 13:27:20 +0530

    Update c.js

--- LeetCode/c.js
+++ LeetCode/c.js
@@ -1,10 +1,10 @@
-let  arr = [1,1,1,1]
-let count =0
-for(let i=0;i<arr.length;i++){
-    for(let j=0;j<arr.length;i++){
-        if(arr[i]==arr[j]){
-            count++
-        }
-    }
-}console.log(count)
-return count
+// Given an array of integers nums, return the number of good pairs.
+
+// A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
