af974edb923acbf08854e1ca30753be5576a818e
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-09-28 13:27:36 +0530

    Update c.js

--- LeetCode/c.js
+++ LeetCode/c.js
@@ -8,3 +8,16 @@
 // Output: 4
 // Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
 
+
+let  arr = [1,1,1,1]
+let count =0
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]==arr[j]){
+          count++
+          
+        }
+    }
+}
+console.log(count)
+return count
