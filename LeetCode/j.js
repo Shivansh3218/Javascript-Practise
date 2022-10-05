6c4e1dbffdc1c1217980bd28de2b2e2e6a04d31b
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-05 22:11:53 +0530

    Update j.js

--- LeetCode/j.js
+++ LeetCode/j.js
@@ -3,3 +3,16 @@
 // Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
 // Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
 
+let image = [[1, 1, 0], [1, 0, 1], [0, 0, 0]];
+let out = []
+let output = []
+for (let i = 0; i < image.length; i++) {
+    for (let j = image[i].length - 1; j >= 0; j--) {
+        if(image[i][j]==0){
+            out.push(1)
+        }else out.push(0)
+    }
+    output.push(out)
+    out = []
+}
+console.log((output))
