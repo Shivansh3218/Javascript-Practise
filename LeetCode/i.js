05db976ecfd45f3d423778ff77619a2ee008ea04
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-05 21:39:27 +0530

    Update i.js

--- LeetCode/i.js
+++ LeetCode/i.js
@@ -1,2 +1,13 @@
 // Input: nums = [1,2,3,3]
 // Output: 3
+let  nums = [1,2,3,3]
+let count=0
+for(let i=0;i<nums.length;i++){
+    for(let j=i+1;j<nums.length;j++){
+        if(nums[i]==nums[j]){
+            count = nums[i]
+        }
+    }
+}
+// return count
+console.log(count)
