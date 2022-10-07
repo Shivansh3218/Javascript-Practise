8f4f48ef70eb6f46b3cc045f0e86edb564e505bd
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-07 22:04:32 +0530

    Update l.js

--- LeetCode/l.js
+++ LeetCode/l.js
@@ -4,3 +4,15 @@
 // Explanation:
 // Product of all elements except current element
 // [2*3*4, 1*3*4, 1*2*4, 1*2*3]
+
+let input =[1,2,3,4]
+let arr = []
+for(let i=0;i<input.length;i++){
+    let pro =1
+    for(let j=0;j<input.length;j++){
+        if(input[j]!=input[i])
+        pro =pro*input[j]
+   }
+   arr.push(pro)
+}
+console.log(arr)
