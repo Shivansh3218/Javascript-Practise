520403f0eff15845351e4599e3ec108847b9f2bd
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-07 21:58:14 +0530

    Create l.js

---
+++ LeetCode/l.js
@@ -0,0 +1,6 @@
+// Guys try to solve this qn when you have time.
+// [1,2,3,4] is input array
+// Expected output [24, 12, 8, 6]
+// Explanation:
+// Product of all elements except current element
+// [2*3*4, 1*3*4, 1*2*4, 1*2*3]
