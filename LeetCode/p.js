7fe8ebc4315ade286fa9ce674b06c149d90e7cd9
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-10 20:05:59 +0530

    Update p.js

--- LeetCode/p.js
+++ LeetCode/p.js
@@ -1,3 +1,17 @@
 // Input: dictionary = ["cat","bat","rat"], sentence = "the cattle was rattled by the battery"
 // Output: "the cat was rat by the bat"
 
+let dictionary =["cat","bat","rat"]
+let sentence ="the cattle was rattled by the battery"
+
+let arr =[]
+let sen =sentence.split(" ")
+for(let i=0;i<dictionary.length;i++){
+    for(let j=0;j<sen.length;j++){
+        if(sen[j].startsWith(dictionary[i])){
+            sen[j] = dictionary[i]
+        }
+    }
+}
+let abc= sen.join(" ")
+console.log( abc)
