5d64b404aa014a6ef774dae714a39450372db9a7
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-28 20:14:35 +0530

    Update e.js

--- Javascript-interview/e.js
+++ Javascript-interview/e.js
@@ -1,20 +1,21 @@
-// function fact(nums){
-//     if(nums==1) return 1
-//     return nums* fact(nums-1)
+function fact(nums){
+    if(nums==1) return 1
+    return nums* fact(nums-1)
     
-// }
-// console.log(fact(5))
+}
+console.log(fact(5))
 
-//CONSTRUCTOR METHOD IN JAVASCRIPT
-// function obj(name,age,gender){
-//        this.name= name
-//         this.age = age
-//         this.gender= gender
-// }
-// let obj1 = new obj("Shivansh", 23, "male")
-// console.log(obj1)
+// CONSTRUCTOR METHOD IN JAVASCRIPT 
 
-//fACTORY METHOD
+function obj(name,age,gender){
+       this.name= name
+        this.age = age
+        this.gender= gender
+}
+let obj1 = new obj("Shivansh", 23, "male")
+console.log(obj1)
+
+// fACTORY METHOD
 function fun(name , age, sex){
     return(
         {
