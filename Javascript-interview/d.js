24cb4b969e32aeadcc18fa68a5d813d658efe48c
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-28 12:32:16 +0530

    Update d.js

--- Javascript-interview/d.js
+++ Javascript-interview/d.js
@@ -1,13 +1,13 @@
 var y = 1;
 if (function f() {}) {
   y += typeof f;
-//   console.log(typeof f)
+  console.log(typeof f)  // Undefined as  function f is not defined anywhere
 }
 console.log(y);
 
 let z=()=>{}
 console.log(z())
-console.log(typeof z)
+console.log(typeof z)  // will give function
 
 function abc(){}
-console.log(typeof abc)
+console.log(typeof abc) // will give function
