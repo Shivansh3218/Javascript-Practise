335f798b8f80107df657e1ddeebce6c72e80f9fd
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-28 12:24:06 +0530

    commit

---
+++ Javascript-interview/b.js
@@ -0,0 +1,6 @@
+function o(){
+    let x = 10
+    return x
+}
+console.log(x) // x is not defined and it's type is undefined
+// same goes for y and also even if you return you cannot log it outside function
