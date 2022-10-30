c4b9af29e8c17583abc99a2c77344d967f0b5682
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-30 11:17:17 +0530

    Create g.js

---
+++ Javascript-interview/g.js
@@ -0,0 +1,26 @@
+// isNaN("hello");
+// console.log(isNaN(10))
+// console.log(Number.isNaN('hello'))
+
+// for (let key in arr) {
+//   console.log(key);
+// }
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// let arr = ["a", ["d","e"], "c"];
+// let newArr = [...arr]
+// newArr[1][0]="f"
+// console.log(arr,newArr)
+
+// useEffect(()=>{
+
+// },[])
+// console.log(isNaN({}))
+let num = 15;
+function names(num){
+return num = 10;
+}
+names()
+console.log(num)
