e8fea2ed9bb8697d1df1aacf198e78b9444a5436
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-31 12:55:00 +0530

    Create new.js

---
+++ Javascript-interview/new.js
@@ -0,0 +1,25 @@
+function getFunc() {
+    let value = "test";
+
+    let func = function () { console.log(value); };
+
+    return func;
+}
+
+getFunc()(); // "test", from the Lexical Environment of getFunc
+
+
+let abc = new Function('a', 'b', 'return a+b')
+console.log(abc(2, 3))
+
+let bc = new Object({
+    a:"eee",
+    b:"cc",
+    d:"dd",
+})
+console.log(bc)
+
+let mul = new Function (nums, nums.forEach(element => {
+   return(element)
+}))
+console.log(mul([1,2,3,4,5,6,7]))
