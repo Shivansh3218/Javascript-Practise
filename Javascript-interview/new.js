ea3f67c10f3fb2d3aadc932c47f78bbc630073c4
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-31 13:00:07 +0530

    Update new.js

--- Javascript-interview/new.js
+++ Javascript-interview/new.js
@@ -1,25 +1,18 @@
 function getFunc() {
     let value = "test";
-
     let func = function () { console.log(value); };
-
     return func;
 }
 
 getFunc()(); // "test", from the Lexical Environment of getFunc
-
-
 let abc = new Function('a', 'b', 'return a+b')
 console.log(abc(2, 3))
 
 let bc = new Object({
-    a:"eee",
-    b:"cc",
-    d:"dd",
+    a: "eee",
+    b: "cc",
+    d: "dd",
 })
 console.log(bc)
-
-let mul = new Function (nums, nums.forEach(element => {
-   return(element)
-}))
-console.log(mul([1,2,3,4,5,6,7]))
+let mul = new Function('a', 'return a.filter((acc)=>acc%2==0)') //must be passed in string
+console.log(mul([1, 2, 3, 4, 5, 6, 7]))
