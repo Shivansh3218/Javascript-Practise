3fe2de3a564fcb9363806f24cf7006590e80bd45
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-10-31 12:54:56 +0530

    Update f.js

--- Javascript-interview/f.js
+++ Javascript-interview/f.js
@@ -2,15 +2,25 @@
 //    let b = await new Promise((res) => {
 //         setTimeout(() => { res("hello") , 2000 })
 //     })
-//      .then((res)=>console.log(res))
 //     return b
 // }
 
-let b = new Promise((res) => {
-    res("hello")
-})
- .then((r)=>{setTimeout(() => { console.log(r), 2000 })})
+// let b = new Promise((res) => {
+//     res("hello")
+// })
+//  .then((r)=>{setTimeout(() => { console.log(r), 2000 })})
 // console.log(abc())
-console.log("ehello world")
-console.log(b)
-console.log("ka")
+// console.log("ehello world")
+// console.log(abc().then((r)=>setTimeout(()=>{console.log(r)})))
+// console.log("ka")
+
+async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return 10;
+  }
+  function f() {
+    // shows 10 after 1 second
+    wait().then(result => console.log(result));
+  }
+  f();
+  console.log("jello")
