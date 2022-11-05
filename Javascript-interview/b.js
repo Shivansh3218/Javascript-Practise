37bcf0efed4bc84d111a899b276b389ba451b74f
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-11-05 11:12:49 +0530

    Practise

--- Javascript-interview/b.js
+++ Javascript-interview/b.js
@@ -1,6 +1,8 @@
-function o(){
-    let x = 10
-    return x
-}
-console.log(x) // x is not defined and it's type is undefined
+// function o(){
+//     let x = 10
+//     return x
+// }
+// console.log(x) // x is not defined and it's type is undefined
 // same goes for y and also even if you return you cannot log it outside function
+
+console.log(isNaN({}))
