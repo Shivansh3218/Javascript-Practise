59d31fd25c2b6816cbe257e1c1cfc32f8c8cdacb
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-11-09 10:38:08 +0530

    Pracitce

---
+++ LeetCode/array.js
@@ -0,0 +1,8 @@
+// let arr =[1,2,3,4,6]
+// let newArr = arr
+// arr[2]="hello"
+// console.log(arr)
+
+
+let a = {b : "c"}
+console.log(typeof(b))
--- LeetCode/mentoring.js
+++ LeetCode/mentoring.js
@@ -1,10 +1,15 @@
 // Print integers one-to-100 but print “Fizz” if an integer is divisible by three, “Buzz” if an integer is divisible by five, and “FizzBuzz” if an integer is divisible by both three and five
 
 let arr=[];
-for(let i=1;i<=100;i++){
-    if(i%3==0) {arr.push("Fizz")}
-    else if(i%5==0){ arr.push("Buzz")}
-    else if(i%3 ==0&&i%5==0){ arr.push("FizzBuzz")}
-    else(arr.push(i))
+// for(let i=1;i<=100;i++){
+//     if(i%3==0) {arr.push("Fizz")}
+//     else if(i%5==0){ arr.push("Buzz")}
+//     else if(i%3 ==0&&i%5==0){ arr.push("FizzBuzz")}
+//     else(arr.push(i))
+// }
+// console.log(arr)
+
+i=0
+switch(i<=100){
+    case i%3==0:
 }
-console.log(arr)
