84986f7b56223cae4ba17a42ae495ec5b3b11f3b
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-11-21 18:47:23 +0530

    Switch revise

---
+++ Javascript-interview/operator.js
@@ -0,0 +1,18 @@
+// console.log(Math.abs(-2.3))
+
+let num = 1
+switch (num){
+    case 0:
+        console.log("monday")
+        break;
+    case 1:
+        console.log("tuesday")
+        break;
+    case 2:
+        console.log("wednesday")
+        break;
+    case 3:
+        console.log("Thursday")
+        break;
+
+}
---
+++ LeetCode/now.js
@@ -0,0 +1,3 @@
+let obj = {name:"sjiv", age:"20"}
+var object = Object.create(obj);
+console.log(object)
