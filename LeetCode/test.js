bd7e9a18743b32a961a56aeee63071dcc89dfd7e
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-11-27 09:42:40 +0530

    Update test.js

--- LeetCode/test.js
+++ LeetCode/test.js
@@ -1,17 +1,19 @@
-let arr = [
-    'e', 't', 'y', 'b', 'r',
-    'e', 'd', 'o', 'c', 'j',
-    '7', 'p', 'd', 'b', 'q',
-    'h', 'e', 'f', '6', '2',
-    '9'
-  ]
-let count = 0
-for (let i = 0; i < arr.length; i++) {
-    if (count === 2) {
-        arr[i] = "X"
-        count = 0
-    }
-    else count++
+// let arr = [
+//     'e', 't', 'y', 'b', 'r',
+//     'e', 'd', 'o', 'c', 'j',
+//     '7', 'p', 'd', 'b', 'q',
+//     'h', 'e', 'f', '6', '2',
+//     '9'
+//   ]
+// let count = 0
+// for (let i = 0; i < arr.length; i++) {
+//     if (count === 2) {
+//         arr[i] = "X"
+//         count = 0
+//     }
+//     else count++
 
-}
-console.log(arr)
+// }
+// console.log(arr)
+
+console.log()
