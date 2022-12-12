1d09415ff8b1a333073c861c20ef9c9de93029a5
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-12-12 22:21:29 +0530

    Memoization practice

---
+++ Javascript-interview/memoized.js
@@ -0,0 +1,35 @@
+const fun = (n) => {
+    let sum = 0
+    for (let i = 0; i <= n; i++) {
+        sum += i
+    }
+    return sum
+}
+
+// console.time()
+// console.log(fun())
+// console.timeEnd()
+
+const memoized = (fun) => {
+    let cache = {}
+    // console.log("heloooo")
+     return function (...args) {
+        // console.log("returning from anonymous function")
+        let n = args[0]
+        if (n in cache) {
+            console.log("cache")
+            return cache[n]
+
+        }
+        else {
+            
+    console.log(cache)
+            let result = fun(n)
+            return result
+        }
+    }
+}
+console.time()
+let efficient = memoized(fun)
+console.log(efficient(5))
+console.timeEnd()
