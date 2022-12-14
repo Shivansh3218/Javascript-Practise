d2db2d5f40adfd9d05f30fcd6dd9f682e64c5bed
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-12-14 19:00:53 +0530

    Create a.js

---
+++ 4-July/a.js
@@ -0,0 +1,29 @@
+// var a = ["a","b"]
+// // a[100]= "z"
+// // console.log(a.length)
+// let b = a.slice()
+// console.log(b)
+// let x =6%2
+// const y =x ?"1":2
+// console.log(y)
+// let v = "aeiou"
+// for(let i=0;i<v.length;i++){
+//     console.log(v[i])
+// }1
+// let a =[1,2 ]
+// a.reverse()
+// console.log(a.pop())
+// const o={
+//   a: 1
+//   ,b:2
+//   ,c:3
+// }
+// const obj = {
+//     ...o,
+//     a:787
+// }
+// console.log(o.a, obj.a)
+
+let abc = {
+    
+}
