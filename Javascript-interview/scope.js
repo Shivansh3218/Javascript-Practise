bd772cadfa321a4f9ee92e503006f4ad2b034293
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-12-29 21:23:11 +0530

    Update scope.js

--- Javascript-interview/scope.js
+++ Javascript-interview/scope.js
@@ -1,9 +1,47 @@
-let fun =(i)=>{
-    setTimeout(()=>{
-        console.log(i)
-       },1000)
-}
-for(var i=0;i<10;i++){ //Switching order will give Reference error that cannot access function before initialization
-     
-    fun(i)
-  }
+// let fun =(i)=>{
+//     setTimeout(()=>{
+//         console.log(i)
+//        },1000)
+// }
+// for(var i=0;i<10;i++){ //Switching order will give Reference error that cannot access function before initialization
+
+//     fun(i)
+//   }
+
+
+// global scope => the variable that you can acess globally
+// functional scope => the variable that you can acess inside a function
+// block scope => the variable that you can acess inside a block
+
+
+//reference error 
+//type error =>
+//syntax error
+
+// function abc() {
+//     {
+//         let a = 10
+//         const b = 20 // blcok scope
+//         var c = 30 // functional scope
+//         d = 40// global scope
+//     }
+//     // console.log( c)
+// }
+// abc()
+// console.log(d)
+// console.log(c)
+// console.log(a)
+
+// primtive and non primitive difference
+
+// let a = 'Shivansh'
+
+// // primitve non mutable
+// // non primitive are mutable
+
+// a[3]= "asdhiasdiasudgiuasgd"
+// // let names = ['s','h','i','v','a']
+// // names[3]= 'asdhiasdiasudgiuasgd'
+// // console.log(a[3])
+// console.log(names[3])
+
